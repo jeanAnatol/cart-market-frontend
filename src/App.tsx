2ad3eaeb31ca83acb93d0893b64db4d931f074b4
YannisAnatolitis <jeannanatol@gmail.com>
2025-12-27 18:23:16 +0200

    feat: hook form dependency and minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
-import AdvertisementsPage from "./components/pages/AdvertisementsPage.tsx";
+// import AdvertisementsPage from "./components/pages/AdvertisementsPage.tsx";
 import Layout from "./components/layout/Layout.tsx";
+import AdvertisementCreateForm from "./components/pages/AdvertisementCreateForm.tsx";
+
 
 
 
@@ -9,7 +11,8 @@ function App() {
   return (
     <>
       <Layout>
-        <AdvertisementsPage />
+        {/*<AdvertisementsPage />*/}
+        <AdvertisementCreateForm />
       </Layout>
     </>
   )
