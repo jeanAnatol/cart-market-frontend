9989109478489296f963058aed361ff3f5770676
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-02 09:15:05 +0200

    feat: implement public api

--- src/services/api.vehicle.catalog.ts
+++ src/services/api.vehicle.catalog.ts
@@ -1,20 +1,21 @@
 // import { vehicleCatalogSchema } from "../schemas/vehicle.catalog.schema.ts";
 import type {FuelTypeDTO, VehicleTypeDTO} from "../types/vehicle.catalog.type.ts";
-import api from "./axios";
+import {publicApi} from "../components/auth/publicApi.ts";
+
 
 
 const referenceUrl = import.meta.env.VITE_SEARCH_API_URL;
 
 export async function fetchVehicleCatalog(): Promise<VehicleTypeDTO[]> {
-  const res = await api.get<VehicleTypeDTO[]>(`${referenceUrl}/all-vehicle-types`);
+  const res = await publicApi.get<VehicleTypeDTO[]>(`${referenceUrl}/all-vehicle-types`);
   return res.data;
 }
 export async function fetchFuelTypes(): Promise<FuelTypeDTO[]> {
-  const res = await api.get<FuelTypeDTO[]>(`${referenceUrl}/all-fuel-types`);
+  const res = await publicApi.get<FuelTypeDTO[]>(`${referenceUrl}/all-fuel-types`);
   return res.data;
 }
 
 export async function fetchVehicleStates(): Promise<string[]> {
-  const res = await api.get<string[]>(`${referenceUrl}/all-vehicle-states`);
+  const res = await publicApi.get<string[]>(`${referenceUrl}/all-vehicle-states`);
   return res.data;
 }
