6c9073f2725b6443c4dc64cac64f9b345e740757
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-02 09:20:55 +0200

    feat: login and register adaptation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@
 import AdvertisementsPage from "./components/pages/AdvertisementsPage.tsx";
 import Layout from "./components/layout/Layout.tsx";
 
-import {BrowserRouter, Routes, Route} from "react-router";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage.tsx";
 import AdvertisementDetailsPage from "./components/pages/AdvertisementDetailsPage.tsx";
 import AdvertisementCreatePage from "./components/pages/AdvertisementCreatePage.tsx";
+import LoginPage from "./components/pages/LoginPage.tsx";
+import ProtectedRoute from "./services/ProtectedRoute.tsx";
+import RegisterPage from "./components/pages/RegisterPage.tsx";
 
 function App() {
 
@@ -15,12 +18,20 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/advertisements/all-ads" element={<AdvertisementsPage />} />
-            <Route path="/advertisements/new" element={<AdvertisementCreatePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/advertisements/all" element={<AdvertisementsPage />} />
             <Route
               path="/advertisements/:uuid"
               element={<AdvertisementDetailsPage />}
             />
+            <Route path="/advertisements/new" element={
+              <ProtectedRoute>
+              <AdvertisementCreatePage />
+              </ProtectedRoute>
+            } />
+            
+            <Route path="/advertisements/:uuid" element={<AdvertisementDetailsPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
