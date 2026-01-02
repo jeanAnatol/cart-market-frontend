c59fc715fae3383d5cf7321fd1abfb6cec5424ef
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-02 09:17:16 +0200

    feat: header logo is clickable and redirects to landing page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,35 @@
+import {Link} from "react-router";
+import LogoutButton from "../LogoutButton.tsx";
 
 const Header = () => {
 
   return (
     <>
-      <header className="bg-blue-400 w-full fixed h-40">
+      <header className="bg-blue-800 w-full fixed top-0 left-0 h-30 z-50 border-b-2 border-blue-400"> {/* <-- z-index-50  */}
         <div className="container flex mx-auto justify-between items-center">
-          <img className="" src="src/assets/logo.png" alt="#"/>
-          <div className="flex gap-4 text-white font-medium">
+          <div className="fixed mt-28 -ml-10">
+          <Link
+            to="/"
+            className=""
+          >
+            <img src="../../../src/assets/logo.png" alt="#"/>
+          </Link>
+          
           
           </div>
-
+          <div className="fixed ml-300 mt-30">
+            <div>
+              <Link className="text-white" to="/login">Login</Link>
+            </div>
+            <div>
+              <Link className="text-white" to="/register">Register</Link>
+            </div>
+          <div>
+            <LogoutButton />
+          </div>
+          
+          
+        </div>
         </div>
       </header>
 
