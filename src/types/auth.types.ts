673dec6db6f7b2ba710b5137d5388789f20f43f1
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-06 12:56:30 +0200

    Feat: new types for user authorization and role recognition

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -13,3 +13,27 @@ export type RegisterRequest = {
   password: string;
   email: string;
 };
+
+export type UserDetailsResponse = {
+  username: string;
+  email: string;
+  role: string;
+}
+
+export type AuthUser = {
+  username: string;
+  role: string;
+};
+
+export type AuthContextType = {
+  user: AuthUser | null;
+  
+  isAuthenticated: boolean;
+  isAdmin: boolean;
+  
+  loading: boolean;
+  
+  login: (token: string) => void;
+  logout: () => void;
+};
+
