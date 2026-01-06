ca19a318eb5260309a02b0246104f7d332dddee2
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-06 18:52:48 +0200

    Fix: move adminHomePage to /pages/admin

--- src/components/pages/admin/AdminHomePage.tsx
+++ src/components/pages/admin/AdminHomePage.tsx
@@ -1,16 +1,11 @@
-import {useAuth} from "../auth/useAuth.ts";
 
 export default function AdminHomePage() {
-  const {user} = useAuth();
   
   return (
     <>
-      <div className="mt-50">
-        <h1 className="text-2xl font-bold mb-4"> Admin Board</h1>
+      <div className="">
+        <h1 className="text-2xl font-bold mb-4 text-orange-300"> Admin Board</h1>
         <div className="bg-white p-6 rounded">
-          <p className="mb-2">
-            Hello {user?.username}
-          </p>
           <p className="text-gray-600">
             This is the admin panel
           </p>
